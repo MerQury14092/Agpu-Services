daf1e572856fb2a3fedf896af9fb184237d0f229
MerQury14092 <petrakov14092@gmail.com>
2023-10-12 19:14:29 +0300

    Refactored TemporarySubscriber.java

--- src/main/java/com/merqury/agpu/timetable/notificatoin/interfaces/TemporarySubscriber.java
+++ src/main/java/com/merqury/agpu/timetable/notificatoin/interfaces/TemporarySubscriber.java
@@ -3,44 +3,57 @@ package com.merqury.agpu.timetable.notificatoin.interfaces;
 
 import com.merqury.agpu.timetable.DTO.Day;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static com.merqury.agpu.AgpuTimetableApplication.*;
 
 public class TemporarySubscriber implements Subscriber {
-    private volatile Day day;
-    private final int seconds;
+    private volatile Day result;
+    private final int secondsForWaitNotification;
     private final String expectedGroupName;
 
-    public TemporarySubscriber(int seconds, String expectedGroupName) {
-        this.seconds = seconds;
+    public TemporarySubscriber(int secondsForWaitNotification, String expectedGroupName) {
+        this.secondsForWaitNotification = secondsForWaitNotification;
         this.expectedGroupName = expectedGroupName;
-        this.day = null;
+        this.result = null;
         async(this::startDaemon);
     }
 
     private void startDaemon(){
+       trySleep(TimeUnit.SECONDS.toMillis(secondsForWaitNotification));
+       setResultToNone();
+    }
+
+    private void trySleep(long milliseconds){
         try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(seconds));
+            Thread.sleep(milliseconds);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        day = Day.builder()
+    }
+
+    private void setResultToNone(){
+        result = Day.builder()
                 .date("NOT")
                 .build();
     }
 
     @Override
     public void handleNotification(String id, Day chagedDay) {
-        if(id.equals(expectedGroupName))
-            day = (Day) chagedDay;
+        if(Objects.equals(id, expectedGroupName))
+            result = chagedDay;
     }
 
     public Day get(){
-        while (day == null)
-            Thread.onSpinWait();
-        if(day.getDate().equals("NOT"))
+        sleepWhileResultEqualsNull();
+        if(Objects.equals(result.getDate(), "NOT"))
             return null;
-        return day;
+        return result;
+    }
+
+    private void sleepWhileResultEqualsNull(){
+        while (result == null)
+            Thread.onSpinWait();
     }
 }
